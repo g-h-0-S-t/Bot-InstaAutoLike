a4d5fc40ee1e1355aa6760a6b4f94ecf33c95acf
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2021-10-14 12:43:22 +0530

    Update Bot-InstaAutoLike.js
    updated selectors and reduced timer to 3 seconds from 10 seconds

--- Bot-InstaAutoLike.js
+++ Bot-InstaAutoLike.js
@@ -1,39 +1,41 @@
-// MIT License
+'use strict';
 
-// Copyright (c) 2021 gh0$t
+/* MIT License
+ * 
+ * Copyright (c) 2021 gh0$t
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. */
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-(function(){
+(function () {
     document.querySelector('.v1Nh3.kIKUG._bz0w > a').click();
-    var likeAndNext = function(){
-        if(document.querySelector('[aria-label="Like"]') && document.querySelector('[aria-label="Like"]').parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode.parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode.parentNode.parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode.parentNode.parentNode.className === 'fr66n'){
-            document.querySelector('[aria-label="Like"]').parentNode.parentNode.parentNode.click();
+    var likeAndNext = function () {
+        if (document.querySelector('[aria-label="Like"]') && document.querySelector('[aria-label="Like"]').parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode.parentNode && document.querySelector('[aria-label="Like"]').parentNode.parentNode.parentNode.className === 'fr66n') {
+            document.querySelector('[aria-label="Like"]').parentNode.parentNode.click();
         }
-        setTimeout(function(){
-            if(document.querySelector('.coreSpriteRightPaginationArrow')){
+        setTimeout(function () {
+            if (document.querySelector('.coreSpriteRightPaginationArrow')) {
                 document.querySelector('.coreSpriteRightPaginationArrow').click();
             }
             else {
                 window.location.reload(true);
             }
-        },1000)
+        }, 1000)
     };
-    setInterval(likeAndNext, 10000);
+    setInterval(likeAndNext, 3000);
 })();
