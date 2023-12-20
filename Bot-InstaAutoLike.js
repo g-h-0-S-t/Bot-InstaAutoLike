ea6f009349643a3d8844b3cebeaf6f44c72e78a3
gh0$t <6196046+g-h-0-S-t@users.noreply.github.com>
2023-12-20 17:14:54 +0530

    Update Bot-InstaAutoLike.js

--- Bot-InstaAutoLike.js
+++ Bot-InstaAutoLike.js
@@ -1,21 +1,19 @@
-javascript:
-
-'use strict';
+javascript: "use strict";
 
 /* MIT License
- * 
+ *
  * Copyright (c) 2021 gh0$t
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,36 +23,42 @@ javascript:
  * SOFTWARE. */
 
 (() => {
-    document.querySelector('._aagw').click();
-    let like = () => {
-        if (document.querySelectorAll('._aamu._ae3_._ae47._ae48 > ._aamw [aria-label="Like"]')[0]) {
-            document.querySelectorAll('._aamu._ae3_._ae47._ae48 > ._aamw [aria-label="Like"]')[0].closest('div[role="button"]').click();
+    const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    const findElement = (selector) => document.querySelector(selector);
+    const clickElement = (element) => element && element.click();
+    const openImage = async () => {
+        const instaImage = findElement("._aagw");
+        if (instaImage) {
+            await wait(1000);
+            clickElement(instaImage);
         }
     };
-    let next = () => {
-        if (document.querySelector('[aria-label="Next"]')) {
-            document.querySelector('[aria-label="Next"]').closest('button').click();
+    const goNextOrClose = async () => {
+        await wait(500);
+        const next = findElement(
+            "[id^='mount_'] > div > div > div.x1n2onr6.x1vjfegm > div > div > div.x78zum5.xdt5ytf.xg6iff7.x1n2onr6.x1ja2u2z > div > div.x6s0dn4.x78zum5.x5yr21d.xl56j7k.x1n2onr6.xh8yej3 > div.x78zum5.xdt5ytf.xch40qd.x1qughib.x10l6tqk.x1nc75zl.xexlgce > div:nth-child(2)"
+        );
+        const close = findElement(
+            "[id^='mount_'] > div > div > div.x1n2onr6.x1vjfegm > div > div > div.x78zum5.xdt5ytf.xg6iff7.x1n2onr6.x1ja2u2z > div > div.x6s0dn4.x78zum5.x5yr21d.xl56j7k.x1n2onr6.xh8yej3 > div.x10l6tqk.x1nc75zl.xi3dyvs.x1mywscw > div"
+        );
+        clickElement(next || close);
+    };
+    const like = async () => {
+        await wait(500);
+        const addHeart = findElement(
+            "[id ^= 'mount_'] > div > div > div.x1n2onr6.x1vjfegm > div > div > div.x78zum5.xdt5ytf.xg6iff7.x1n2onr6.x1ja2u2z > div > div.x6s0dn4.x78zum5.x5yr21d.xl56j7k.x1n2onr6.xh8yej3 > div.x1y0lptx.x78zum5.xcdomlo.xdt5ytf.x10l6tqk.x100vrsf > div.html-div.xe8uvvx.xdj266r.x11i5rnm.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x6s0dn4.x1ypdohk.x78zum5.xdt5ytf.xieb3on > span > div"
+        );
+        const addHeartFlag =
+            addHeart && addHeart.querySelector('svg[aria-label="Like"]');
+        if (addHeartFlag) {
+            clickElement(addHeart);
         }
     };
-    let close = () => {
-        if (document.querySelector('[aria-label="Close"]') && !document.querySelector('[aria-label="Next"]')) {
-            document.querySelector('[aria-label="Close"]').closest('div').click();
-            let href = window.location.href;
-            window.location.href = href + '#';
-        }
+    const likeAndNext = async () => {
+        await like();
+        await goNextOrClose();
+        setTimeout(likeAndNext, 500);
     };
-    let alreadyLiked = () => document.querySelector('._aamu._ae3_._ae47._ae48 > ._aamw [aria-label="Unlike"]');
-    let likeAndNext = () => {
-        like();
-        setTimeout(() => {
-            if (alreadyLiked()) {
-                next();
-                close();
-            } else {
-                like();
-            }
-        }, 1000);
-        setTimeout(likeAndNext, 1000);
-    };
-    likeAndNext();
+    openImage();
+    setTimeout(likeAndNext, 1000);
 })();
